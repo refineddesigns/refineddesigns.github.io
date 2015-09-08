0ecabb67ee23400522ef4dca5796bf4a3a519b6e
refineddesigns <simone.greentree@hotmail.com>
2015-09-08 21:09:32 +1000

    Google analytics tweak.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ function toggleChevron(e) {
 $('#accordion').on('hidden.bs.collapse', toggleChevron);
 $('#accordion').on('shown.bs.collapse', toggleChevron);
 
-<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -19,5 +18,3 @@ $('#accordion').on('shown.bs.collapse', toggleChevron);
 
   ga('create', 'UA-67307257-1', 'auto');
   ga('send', 'pageview');
-
-</script>
